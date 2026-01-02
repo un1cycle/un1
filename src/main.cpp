849b139e9e23e6cd2c42f999a2913091b913437c
eshon <eshaan2031@icloud.com>
2026-01-02 00:14:56 -0600

    added control flow abstractions

--- src/main.cpp
+++ src/main.cpp
@@ -170,65 +170,126 @@ pratt_parse(std::unique_ptr<Node> root, const std::vector<token> &buffer_lex) {
 
 std::expected<std::unique_ptr<Node>, parse_error>
 parse(std::unique_ptr<Node> root, const std::vector<token> &buffer_lex,
+      int index_begin);
+
+std::expected<std::unique_ptr<Node>, parse_error> handle_control_flow_keyword(
+    std::unique_ptr<Node> root, const std::vector<token> &buffer_lex,
+    int index_begin, int i,
+    const std::string &keyword) { // i means current index
+  std::vector<token> temp_buffer = {};
+  i++;
+  while (buffer_lex[i].value != "{") {
+    if (i >= buffer_lex.size())
+      return std::unexpected(parse_error::UNBALANCED_BRACES);
+    temp_buffer.push_back(
+        buffer_lex[i]); // means you didn't open if statement code
+    i++;
+  } // now buffer_lex[i].value == "{"
+
+  root->attributes = {
+      keyword}; // root->left->left should be conditional, root->left->right
+                // should be code, root->right should be rest of code
+  root->left = std::make_unique<Node>(
+      root.get(), std::make_unique<Node>(nullptr, nullptr, nullptr),
+      std::make_unique<Node>(nullptr, nullptr, nullptr));
+  auto conditional_result =
+      pratt_parse(std::move(root->left->left), temp_buffer);
+  if (!conditional_result.has_value()) {
+    return std::unexpected(conditional_result.error());
+  }
+
+  root->left->left = std::move(*conditional_result);
+
+  temp_buffer = {};
+  i++;
+  int bracket_count = 1;
+  while (bracket_count != 0) {
+    if (i >= buffer_lex.size())
+      return std::unexpected(parse_error::UNBALANCED_BRACES);
+    if (buffer_lex[i].value == "{") // means overstepped buffer length
+      bracket_count++;
+    else if (buffer_lex[i].value == "}")
+      bracket_count--;
+
+    temp_buffer.push_back(buffer_lex[i]);
+    i++;
+  } // now buffer_lex[    i - 1   ].value == "}"
+
+  auto code_result = parse(std::move(root->left->right), temp_buffer, 0);
+  if (!code_result.has_value()) {
+    return std::unexpected(code_result.error());
+  }
+  root->left->right = std::move(*code_result);
+
+  root->right = std::make_unique<Node>(nullptr, nullptr, nullptr);
+  auto later_code_result = parse(std::move(root->right), buffer_lex, i);
+  if (!later_code_result.has_value()) {
+    return std::unexpected(later_code_result.error());
+  }
+  root->right = std::move(*later_code_result);
+
+  return std::move(root);
+}
+
+std::expected<std::unique_ptr<Node>, parse_error>
+handle_all_control_flow_keywords(std::unique_ptr<Node> root,
+                                 const std::vector<token> &buffer_lex,
+                                 int index_begin,
+                                 int i) { // i means current index
+  if (buffer_lex[i].type == lex_types::WORD && buffer_lex[i].value == "if") {
+    auto temp_root = handle_control_flow_keyword(std::move(root), buffer_lex,
+                                                 index_begin, i, "if");
+    if (!temp_root.has_value())
+      return std::unexpected(temp_root.error());
+    root = std::move(*temp_root);
+  }
+
+  else if (buffer_lex[i].type == lex_types::WORD &&
+           buffer_lex[i].value == "while") {
+    auto temp_root = handle_control_flow_keyword(std::move(root), buffer_lex,
+                                                 index_begin, i, "while");
+    if (!temp_root.has_value())
+      return std::unexpected(temp_root.error());
+    root = std::move(*temp_root);
+  } else if (buffer_lex[i].type == lex_types::WORD &&
+             buffer_lex[i].value == "for") {
+    auto temp_root = handle_control_flow_keyword(std::move(root), buffer_lex,
+                                                 index_begin, i, "for");
+    if (!temp_root.has_value())
+      return std::unexpected(temp_root.error());
+    root = std::move(*temp_root);
+  }
+
+  else if (buffer_lex[i].type == lex_types::WORD &&
+           buffer_lex[i].value == "else") {
+    auto temp_root = handle_control_flow_keyword(std::move(root), buffer_lex,
+                                                 index_begin, i, "else");
+    if (!temp_root.has_value())
+      return std::unexpected(temp_root.error());
+    root = std::move(*temp_root);
+  } else if (buffer_lex[i].type == lex_types::WORD &&
+             buffer_lex[i].value == "elif") {
+    auto temp_root = handle_control_flow_keyword(std::move(root), buffer_lex,
+                                                 index_begin, i, "elif");
+    if (!temp_root.has_value())
+      return std::unexpected(temp_root.error());
+    root = std::move(*temp_root);
+  }
+  return std::move(root);
+}
+
+std::expected<std::unique_ptr<Node>, parse_error>
+parse(std::unique_ptr<Node> root, const std::vector<token> &buffer_lex,
       int index_begin) {
   for (int i = index_begin; i < buffer_lex.size(); ++i) {
-    // C doesn't allow {} in its conditionals meaning that we can just use that
-    // as a range
-    if (buffer_lex[i].type == lex_types::WORD && buffer_lex[i].value == "if") {
-      std::vector<token> temp_buffer = {};
-      i++;
-      while (buffer_lex[i].value != "{") {
-        if (i >= buffer_lex.size())
-          return std::unexpected(parse_error::UNBALANCED_BRACES);
-        temp_buffer.push_back(
-            buffer_lex[i]); // means you didn't open if statement code
-        i++;
-      } // now buffer_lex[i].value == "{"
+    // C doesn't allow {} in its conditionals meaning that we can just use
+    // that as a range
+    auto temp_root = handle_all_control_flow_keywords(
+        std::move(root), buffer_lex, index_begin, i);
 
-      root->attributes = {
-          "if"}; // root->left->left should be conditional, root->left->right
-                 // should be code, root->right should be rest of code
-      root->left = std::make_unique<Node>(
-          root.get(), std::make_unique<Node>(nullptr, nullptr, nullptr),
-          std::make_unique<Node>(nullptr, nullptr, nullptr));
-      auto conditional_result =
-          pratt_parse(std::move(root->left->left), temp_buffer);
-      if (!conditional_result.has_value()) {
-        return std::unexpected(conditional_result.error());
-      }
-
-      root->left->left = std::move(*conditional_result);
-
-      temp_buffer = {};
-      i++;
-      int bracket_count = 1;
-      while (bracket_count != 0) {
-        if (i >= buffer_lex.size())
-          return std::unexpected(parse_error::UNBALANCED_BRACES);
-        if (buffer_lex[i].value == "{") // means overstepped buffer length
-          bracket_count++;
-        else if (buffer_lex[i].value == "}")
-          bracket_count--;
-
-        temp_buffer.push_back(buffer_lex[i]);
-        i++;
-      } // now buffer_lex[    i - 1   ].value == "}"
-
-      auto code_result = parse(std::move(root->left->right), temp_buffer, 0);
-      if (!code_result.has_value()) {
-        return std::unexpected(code_result.error());
-      }
-      root->left->right = std::move(*code_result);
-
-      root->right = std::make_unique<Node>(nullptr, nullptr, nullptr);
-      auto later_code_result = parse(std::move(root->right), buffer_lex, i);
-      if (!later_code_result.has_value()) {
-        return std::unexpected(later_code_result.error());
-      }
-      root->right = std::move(*later_code_result);
-
-      return std::move(root);
-    }
+    if (!temp_root.has_value())
+      return std::unexpected(temp_root.error());
+    root = std::move(*temp_root);
   }
   return std::unexpected(parse_error::NO_MATCH_FOUND);
 }
